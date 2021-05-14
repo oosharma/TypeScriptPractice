5aefe0a93b495a48523777df89b4a6b88bf7fa32
Abhishek Sharma <abhisheksharma@abhisheks-mbp-2.lan>
2021-05-13 17:47:09 -0700

    Added unknown type.

--- index.ts
+++ index.ts
@@ -11,3 +11,16 @@ let fruit: {
 }
 
 const {name: string} = fruit
+
+// Unkown can take any types but TS limits the things we can do with it
+const unknownString: unknown = "What am I?"
+
+// Can not execute the following statement
+// console.log(unknownString.toUpperCase()) 
+
+//We can do type narrowing to do runtime checks
+if (typeof unknownString === "string"){
+    console.log(unknownString.toUpperCase())
+}
+
+// any gives more flexibility than unknown, but unknown gives more type safety
